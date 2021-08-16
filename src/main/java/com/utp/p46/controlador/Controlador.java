945c8507e7f1cc8a6f9816e318e7b24ff2a49e51
sean <seandaza@gmail.com>
2021-08-16 18:53:16 -0500

    update

--- src/main/java/com/utp/p46/controlador/Controlador.java
+++ src/main/java/com/utp/p46/controlador/Controlador.java
@@ -1,5 +1,22 @@
 package com.utp.p46.controlador;
 
+import java.sql.Connection;
+
 public class Controlador {
-    
+    /***********
+     * Atributos
+     ***********/  
+     private Connection conn;
+     private Vista vista;
+
+    /***********
+     * Constructor
+     ***********/
+
+     public Controlador(Vista vista){
+         this.vista = vista;
+     }
+    /***********
+     * Metodos
+     ***********/
 }
