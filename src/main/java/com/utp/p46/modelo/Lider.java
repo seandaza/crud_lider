4e42937693a2dde8243b93c21b2b8d5458640012
sean <seandaza@gmail.com>
2021-08-15 23:06:16 -0500

    update

--- src/main/java/com/utp/p46/modelo/Lider.java
+++ src/main/java/com/utp/p46/modelo/Lider.java
@@ -1,5 +1,118 @@
 package com.utp.p46.modelo;
 
 public class Lider {
+    /***********
+     * Atributos
+     ***********/
+    private int id;
+    private String nombre;
+    private String primer_apellido;
+    private String segundo_apellido;
+    private int salario;
+    private String ciudad_residencia;
+    private String cargo;
+    private String clasificacion;
+    private String documento_identidad;
+    private String fecha_nacimiento;
+
+    /***********
+     * Constructores
+     ***********/
+    public Lider() {
+
+    }
+
+    public Lider(int id, String nombre, String primer_apellido, String segundo_apellido, int salario, String ciudad_residencia, String cargo, String clasificacion, String documento_identidad, String fecha_nacimiento){
+        this.id = id;
+        this.nombre = nombre;
+        this.primer_apellido = primer_apellido;
+        this.segundo_apellido = segundo_apellido;
+        this.salario = salario;
+        this.ciudad = ciudad_residencia;
+        this.cargo = cargo;
+        this.clasificacion = clasificacion;
+        this.documento_identidad = documento_identidad;
+        this.fecha_nacimiento = fecha_nacimiento;
+
+    }
+    
+    /***********
+     * Getters and Setters
+     ***********/
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre (String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPrimer_apellido() {
+        return primer_apellido;
+    }
+
+    public void setPrimer_apellido(String primer_apellido) {
+        this.primer_apellido = primer_apellido;
+    }
+
+    public String getSegundo_apellido() {
+        return segundo_apellido;
+    }
+
+    public void setSegundo_apellido(String segundo_apellido) {
+        this.segundo_apellido = segundo_apellido;
+    }
+
+    public int getSalario() {
+        return salario;
+    }
+
+    public void setSalario(int salario) {
+        this.salario = salario;
+    }
+
+    public String getCiudad_residencia() {
+        return ciudad_residencia;
+    }
+
+    public void setCiudad_residencia(String ciudad_residencia) {
+        this.ciudad_residencia = ciudad_residencia;
+    }
+
+    public String getClasificacion() {
+        return clasificacion;
+    }
+
+    public void SetClasificiacion(String clasificacion) {
+        this.clasificacion = clasificacion;
+    }
+
+    public String getDocumento_identidad() {
+        return documento_identidad;
+    }
+
+    public void setDocumento_identidad(String documento_identidad) {
+        this.documento_identidad = documento_identidad;
+    }
+
+    public String getFecha_nacimiento() {
+        return fecha_nacimiento;
+    }
+
+    public void setFecha_nacimiento(String fecha_nacimiento){
+        this.fecha_nacimiento = fecha_nacimiento;
+    }
+
+
+
+
 
 }
